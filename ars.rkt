0de6b7006ad4f38525e1aef04f24d3f36127af82
Guthrie Price <guthrie.price@bmahq.com>
2022-03-03 10:48:13 -0800

    finish parsing ars query results

--- ars.rkt
+++ ars.rkt
@@ -8,6 +8,7 @@
 
 (require racket/match
          racket/pretty
+         racket/file
          net/http-client
          json)
 
@@ -16,7 +17,6 @@
 (define ars-host "ars.transltr.io")
 (define post-query-uri "/ars/api/submit")
 (define pull-query-uri "/ars/api/messages/~a~a")
-
 
 (define (ars-sendrecv uri (method #"GET") (data #f))
   (match-define-values (_ _ resp-out)
@@ -29,13 +29,17 @@
 
 (define (get-code health) (car health))
 (define (get-status health) (cdr health))
-(define (get-health resp) (car resp))
+(define (query-done? health)
+  (and (equal? (get-code health)   200)
+       (equal? (get-status health) "Done")))
+(define (parse-health jse)
+  (let ((code (jsexpr-object-ref jse 'code))
+        (status (jsexpr-object-ref jse 'status)))
+    (cons code status)))
+
 
 (define (parse-query-status resp)
-  (let* ((fields (jsexpr-object-ref resp   'fields))
-         (code   (jsexpr-object-ref fields 'code))
-         (status (jsexpr-object-ref fields 'status)))
-    (cons code status)))
+  (parse-health (jsexpr-object-ref resp 'fields)))
 
 (define (parse-submit-query-resp resp)
   (let* ((health (parse-query-status resp))
@@ -43,6 +47,16 @@
     (cons health qid)))
 
 (define (parse-query-result resp)
+  (let ((status (jsexpr-object-ref resp 'status)))
+    (if (equal? status "Done")
+        (for/fold ((actor-data '()))
+                  ((actor (jsexpr-object-ref resp 'children)))
+          (if (query-done? (parse-health actor))
+              (cons (pull-query-actor-result (jsexpr-object-ref actor 'message)) actor-data)
+              actor-data))
+        #f))) ;TODO: exception?
+
+(define (parse-query-actor-result resp)
   resp)
 
 (define (post-query query)
@@ -61,6 +75,14 @@
 (define (pull-query-result qid)
   (parse-query-result (pull-query qid #t)))
 
+(define (pull-query-actor-result qid)
+  (parse-query-actor-result (pull-query qid)))
+
 ;(pretty-print (post-query (read-json (open-input-file "test/workflowA/A.0_RHOBTB2_direct.json"))))
 ;(pretty-print (pull-query-status "490236a7-ec86-4776-9a4d-d8d117b6df9f"))
-(pretty-print (pull-query-result "490236a7-ec86-4776-9a4d-d8d117b6df9f"))
+(write-json
+  (pull-query-result "490236a7-ec86-4776-9a4d-d8d117b6df9f")
+  (open-output-file "./test/workflowA/resp/A.0_RHOBTB2_direct_children.json" #:exists 'replace))
+#;(write-json
+  (pull-query-actor-result "ada9d2b3-2a75-47f9-86e3-dbb295c1a0bb")
+  (open-output-file "./A.0_RHOBTB2_direct_aragorn.json" #:exists 'replace))
