84faaceb9b09cf4908f6c0560f80fcec936063f7
Guthrie Price <guthrie.price@bmahq.com>
2022-03-23 14:00:10 -0700

    make curie searcher mockable in trapi

--- trapi.rkt
+++ trapi.rkt
@@ -16,18 +16,18 @@
 (define (index->edge-id i) (string-add-prefix "e" (number->string i)))
 (define (biolink-tag str) (string-add-prefix "biolink:" str))
 
-(define (qnode->trapi-qnode qnode)
+(define (qnode->trapi-qnode qnode curie-searcher)
   (if (empty-string? (jsexpr-object-ref qnode 'name))
       (hasheq 'categories `(,(biolink-tag (jsexpr-object-ref qnode 'type))))
-      (hasheq 'ids (curie-search nr-searcher (jsexpr-object-ref qnode 'name) 0 10)
+      (hasheq 'ids        (curie-searcher (jsexpr-object-ref qnode 'name))
               'categories `(,(biolink-tag (jsexpr-object-ref qnode 'type))))))
 
 (define (qedge->trapi-qedge qedge)
-  (hasheq 'subject (index->node-id (jsexpr-object-ref qedge 'source))
-          'object  (index->node-id (jsexpr-object-ref qedge 'target))
+  (hasheq 'subject    (index->node-id (jsexpr-object-ref qedge 'source))
+          'object     (index->node-id (jsexpr-object-ref qedge 'target))
           'predicates `(,(biolink-tag (jsexpr-object-ref qedge 'type)))))
   
-(define (qgraph->trapi-qgraph qgraph)
+(define (qgraph->trapi-qgraph qgraph curie-searcher)
   (with-handlers ((exn:fail:contract? (lambda (e) #f)))
     (define (objs->trapi-objs key id-generator obj-converter)
       (let loop ((os (jsexpr-object-ref qgraph key))
@@ -39,11 +39,14 @@
                   (+ i 1)
                   (cons `(,(string->symbol (id-generator i)) . ,(obj-converter (car os)))
                         trapi-os)))))
-    (hasheq 'nodes (objs->trapi-objs 'nodes index->node-id qnode->trapi-qnode) 
+    (hasheq 'nodes (objs->trapi-objs 'nodes index->node-id (lambda (qnode)
+                                                            (qnode->trapi-qnode qnode curie-searcher))) 
             'edges (objs->trapi-objs 'edges index->edge-id qedge->trapi-qedge))))
 
-(define (qgraph->trapi-query qgraph)
-  (define trapi-qgraph (qgraph->trapi-qgraph qgraph))
+(define (qgraph->trapi-query qgraph (curie-searcher
+                                      (lambda (name)
+                                        (curie-search nr-searcher name 0 10))))
+  (define trapi-qgraph (qgraph->trapi-qgraph qgraph curie-searcher))
   (and trapi-qgraph
        (hasheq 'message (hasheq 'query_graph trapi-qgraph))))
     
@@ -51,9 +54,10 @@
 (module+ test
   (require rackunit)
   (define test-qgraph (read-json (open-input-file "test/trapi/qgraph.json")))
-  (define test-qgraph-expected (read-json (open-input-file "test/trapi/qgraph-expected.json")))
-  (check-equal? (qgraph->trapi-qgraph test-qgraph) test-qgraph-expected)
+  (define test-query-expected (read-json (open-input-file "test/trapi/query-expected.json")))
+  (define (mock-curie-searcher name) '("NCBIGene:23221" "MONDO:0033373" "UMLS:C4693903"))
+  (check-equal? (qgraph->trapi-query test-qgraph mock-curie-searcher) test-query-expected)
 
   (define test-invalid-qgraph (read-json (open-input-file "test/trapi/invalid-qgraph.json")))
-  (check-false (qgraph->trapi-qgraph test-invalid-qgraph))
+  (check-false (qgraph->trapi-query test-invalid-qgraph))
 )
