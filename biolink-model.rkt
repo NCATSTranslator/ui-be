20a434b1730711c9dfde7b935b0bf12b04e614bc
Nishad Prakash <nishad@gmail.com>
2022-08-01 16:45:34 -0400

    Move to reading in JSON: much faster than reading yaml

--- biolink-model.rkt
+++ biolink-model.rkt
@@ -1,17 +1,31 @@
 #lang racket/base
 (require yaml)
+(require json)
+(require memo)
 
-(define y (file->yaml "b.yaml"))
-(define slots (hash-ref y "slots"))
+
+;(define y (file->yaml "b.yaml"))     ; 15 fucking seconds!
+; (define slots (hash-ref y "slots"))
+
+(define j (with-input-from-file "biolink-model.json" (lambda () (read-json))))
+(define slots (hash-ref j 'slots))
 
 (define (my-hash-map-filter hash map-proc filter-proc)
   (filter filter-proc (hash-map hash map-proc)))
 ; E.g. usage: (my-hash-map-filter slots (lambda (k v) (cons k (hash-has-key? v "is_a"))) (lambda (x) (not (cdr x))))
 
+(define/memoize (is-child-of-related-to slots-hash predicate)
+  (let ((cur (hash-ref slots-hash predicate #f)))        
+    (cond
+      ((not cur) #f)
+      ((eq? predicate 'related\ to) #t) ; put this first
+      ((not (hash-has-key? cur 'is_a)) #f)
+      (else (is-child-of-related-to slots-hash (string->symbol (hash-ref cur 'is_a)))))))
+
 (define slot-graph (make-hash))
 (define inverses (make-hash))
 
-(define (traverse-hash hash slot-graph inverses)
+(define (build-slot-maps hash slot-graph inverses)
   (hash-for-each
    hash
    (lambda (k v)
