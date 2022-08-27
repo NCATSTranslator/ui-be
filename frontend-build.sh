bd9a087d526939bddc8e6d910dcc310834862513
Nishad Prakash <nishad@gmail.com>
2022-08-27 18:30:42 -0400

    Add option to frontend-build.sh to not do npm stuff

--- frontend-build.sh
+++ frontend-build.sh
@@ -2,25 +2,42 @@
 
 ui_repo="git@github.com:dnsmith124/ui-prototype-one.git"
 ui_src="ui-prototype-one"
-fe_branch=${1}
-if [ -z "${fe_branch}" ]; then
-  fe_branch="develop"
+
+if [ $# -eq 0 ]; then
+    fe_branch="develop"
+    do_build="yes"
+elif [ $# -eq 1 ]; then
+    if [ "${1}" = "nobuild" ]; then
+        fe_branch="develop"
+        do_build="nobuild"
+    else
+        fe_branch="${1}"
+        do_build="yes"
+    fi
+else # $# == 2
+    fe_branch="${1}"
+    do_build="${2}"
 fi
 
 echo "Starting UI build"
 
 echo "Downloading front end"
 git clone ${ui_repo}
-echo "Building front end"
+echo "Cloning front end"
 pushd ${ui_src} > /dev/null
 git checkout ${fe_branch}
 git pull
-echo "Installing front end dependencies"
-npm install
-echo "Building front end source"
-npm run build
-rsync -av build ..
+if [ "${do_build}" != "nobuild" ]; then 
+    echo "Installing front end dependencies"
+    npm install
+    echo "Building front end source"
+    npm run build
+    rsync -av build ..
+    echo "Done building front end"
+else
+    echo "Skipping npm build process"
+fi
 popd > /dev/null
-echo "Done building front end"
+
 
 echo "Done building UI"
