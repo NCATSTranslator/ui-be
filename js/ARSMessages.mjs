0dbdbc0d07f978a2c5727529eec4dcdfdba6a644
Nishad Prakash <nishad@gmail.com>
2023-01-09 16:56:32 -0800

    Add some helpful methods

--- js/ARSMessages.mjs
+++ js/ARSMessages.mjs
@@ -1,11 +1,25 @@
 'use strict';
 
-export {isAcceptedQuery, pk} ;
+export { isAcceptedQuery, msgId, parentQueryRunning,parentQueryDone }
 
 function isAcceptedQuery(msg) {
     return msg.fields.code === 200 || msg.fields.code === 202;
 }
 
-function pk(msg) {
-    return msg.pk;
+function msgId(msg) {
+    if (msg.hasOwnProperty('message')) {
+        return msg.message;
+    } else if (msg.hasOwnProperty('pk')) {
+        return 'pk';
+    } else {
+        return null;
+    }
+}
+
+function parentQueryRunning(msg) {
+    msg.status === 'Running';
+}
+
+function parentQueryDone(msg) {
+    msg.status === 'Done';
 }
