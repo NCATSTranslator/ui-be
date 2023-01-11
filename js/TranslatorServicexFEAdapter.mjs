a1c07ebd4bb68626835e20aa3b41b39e4d2bfe12
Nishad Prakash <nishad@gmail.com>
2023-01-10 21:30:22 -0800

    Add a format adapter between TranslatorService and the FE

---
+++ js/TranslatorServicexFEAdapter.mjs
@@ -0,0 +1,43 @@
+'use strict';
+
+import * as arsmsg from './ARSMessages.mjs';
+
+/* Translate messages coming from the Translator Service into the formats that the Frontend (FE) app expects */
+/* This module should not contain logic that goes beyond message transformations */
+export { queryStatusToFE, queryResultsToFE, querySubmitToFE };
+// msg: ARS client message with trace=y
+function queryStatusToFE(msg) {
+    return {
+        status: msg.running.length > 0 ? "running" : "success",
+        data: {
+            qid: msg.pk,
+            aras: msg.completed.map(e => e.agent)
+        }
+    };
+}
+
+// msg: an ARS client message w/ results
+function queryResultsToFE(msg) {
+    // Omit ARA results where the actual results array is empty
+    // Need to account for the ARS returning both null and []
+    let data = msg.completed.filter(e => Array.isArray(e.data.results) && e.data.results.length > 0).map(
+        e => { return {
+                agent: e.agent,
+                message: e.data
+            }
+        });
+
+    return {
+        status: msg.running.length > 0 ? "running" : "success",
+        data: data
+    };
+}
+
+// Currently the FE doesn't expect this message to be handling failure conditions
+// msg: The standard ARS message returned when you post a query
+function querySubmitToFE(msg) {
+    return {
+        status: 'success',
+        data: arsmsg.msgId(msg)
+    }
+}
