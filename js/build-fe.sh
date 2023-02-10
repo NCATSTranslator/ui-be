53183d3f91d650185ba8f972b3a24daba14b651b
Nishad Prakash <nishad@gmail.com>
2023-02-09 21:35:17 -0500

    Add all files from one level up, here

---
+++ js/build-fe.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <fe-branch-name> <local|dev|ci|test|production> <yes|no>"
+    exit 1
+fi
+
+fe_repo="https://github.com/NCATSTranslator/ui-fe.git"
+fe_src="ui-fe"
+fe_branch="${1}"
+app_env="${2}"
+do_build="${3}"
+echo "Starting UI build"
+if [ ! -d "${fe_src}" ]; then
+   echo "Cloning front end"
+   git clone ${fe_repo}
+fi
+cd ${fe_src}
+git checkout ${fe_branch}
+git pull
+if [ "${do_build}" = "yes" ]; then
+    echo "Installing front end dependencies"
+    npm install
+    echo "Building front end source"
+    npm run build:$app_env
+    echo "Done building front end"
+else
+    echo "Skipping npm build process"
+fi
+cd ..
+echo "Done"
