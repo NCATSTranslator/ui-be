986b86009b7af597280ccbec2aed2ea14ac679ec
Nishad Prakash <nishad@gmail.com>
2023-06-26 08:04:18 -0400

    Add a utility pgExec function and use it

--- postgres_preamble.mjs
+++ postgres_preamble.mjs
@@ -3,10 +3,26 @@
 // This worked after a bit of trial and error.
 import pg from 'pg';
 
-export { pg };
+export { pg, pgExec };
 
 /* Technically unsafe due to integer precision in node but probably fine in reality.
  * Allows count() results to be returned as numbers instead of strings.
  *https://github.com/brianc/node-pg-types#use
  */
 pg.types.setTypeParser(pg.types.builtins.INT8, parseInt);
+
+async function pgExec(pool, sql, sqlParams=[]) {
+  let client = null;
+  try {
+    client = await pool.connect();
+    const res = await client.query(sql, sqlParams);
+    return res;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    if (client) {
+      client.release();
+    }
+  }
+}
--- users/UserStorePostgres.mjs
+++ users/UserStorePostgres.mjs
@@ -1,6 +1,6 @@
 'use strict';
 
-import { pg } from '../postgres_preamble.mjs';
+import { pg, pgExec } from '../postgres_preamble.mjs';
 import { User } from '../models/User.mjs';
 
 export { UserStorePostgres };
@@ -11,117 +11,63 @@ class UserStorePostgres {
   }
 
   async retrieveUserById(id) {
-    let client = null;
     let retval = null;
-    try {
-      client = await this.pool.connect();
-      const res = await client.query('SELECT * FROM users WHERE id = $1', [id]);
-      if (res.rows.length > 0) {
+    const res = await pgExec(this.pool, 'SELECT * FROM users WHERE id = $1', [id]);
+    if (res.rows.length > 0) {
         retval = new User(res.rows[0]);
       }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-      return retval;
-    }
+    return retval;
   }
-
 
   async retrieveUserByEmail(email) {
-    let client = null;
     let retval = null;
-    try {
-      client = await this.pool.connect();
-      const res = await client.query('SELECT * FROM users WHERE email = $1', [email]);
-      if (res.rows.length > 0) {
+    const res = await pgExec(this.pool, 'SELECT * FROM users WHERE email = $1', [email]);
+    if (res.rows.length > 0) {
         retval = new User(res.rows[0]);
       }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-      return retval;
-    }
+    return retval;
   }
 
-  async createNewUser(userData) {
-    let client = null;
+  async createNewUser(user) {
     let retval = null;
-    try {
-      client = await this.pool.connect();
-      const res = await client.query(`
-        INSERT INTO users (id, name, email, time_created, time_updated, profile_pic_url, data)
-        VALUES ($1, $2, $3, $4, $5, $6, $7)
-        RETURNING *
-      `, [userData.id, userData.name, userData.email, userData.time_created,
-          userData.time_updated, userData.profile_pic_url, userData.data]);
-      if (res.rows.length > 0) {
-        retval = new User(res.rows[0]);
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-      return retval;
+    const res = await pgExec(this.pool, `
+      INSERT INTO users (id, name, email, time_created, time_updated, profile_pic_url, data)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *
+    `, [user.id, user.name, user.email, user.time_created,
+        user.time_updated, user.profile_pic_url, user.data]);
+    if (res.rows.length > 0) {
+      retval = new User(res.rows[0]);
     }
+    return retval;
   }
 
-  async updateUser(userData) {
+  async updateUser(user) {
     let retval = null;
-    let client = null;
-    if (!userData.time_updated) {
-      userData.updateUpdatedTime();
+    const res = await pgExec(this.pool, `
+      UPDATE users
+      SET name = $2, email = $3, time_created = $4, time_updated = $5, profile_pic_url = $6, data = $7
+      WHERE id = $1
+      RETURNING *
+    `, [user.id, user.name, user.email, user.time_created,
+        user.time_updated, user.profile_pic_url, user.data]);
+    if (res.rows.length > 0) {
+      retval = new User(res.rows[0]);
     }
-    try {
-      client = await this.pool.connect();
-      const res = await client.query(`
-        UPDATE users
-        SET name = $2, email = $3, time_created = $4, time_updated = $5, profile_pic_url = $6, data = $7
-        WHERE id = $1
-        RETURNING *
-      `, [userData.id, userData.name, userData.email, userData.time_created,
-          userData.time_updated, userData.profile_pic_url, userData.data]);
-      if (res.rows.length > 0) {
-        retval = new User(res.rows[0]);
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-      return retval;
-    }
+    return retval;
   }
 
   async updateUpdatedTime(id, datetime = new Date()) {
     let retval = null;
-    let client = null;
-    try {
-      client = await this.pool.connect();
-      const res = await client.query(`
-        UPDATE users
-        SET time_updated = $2
-        WHERE id = $1
-        RETURNING *
-      `, [id, datetime]);
-      if (res.rows.length > 0) {
-        retval = new User(res.rows[0]);
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (client) {
-        client.release();
-      }
-      return retval;
+    const res = await pgExec(this.pool, `
+      UPDATE users
+      SET time_updated = $2
+      WHERE id = $1
+      RETURNING *
+    `, [id, datetime]);
+    if (res.rows.length > 0) {
+      retval = new User(res.rows[0]);
     }
+    return retval;
   }
 }
