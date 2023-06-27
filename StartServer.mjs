4eb4bc6b6abf1b85d00c071bc476674df65b2dcf
Nishad Prakash <nishad@gmail.com>
2023-06-27 19:24:46 -0400

    Add the user store into the Auth service

--- StartServer.mjs
+++ StartServer.mjs
@@ -10,6 +10,7 @@ import { KGAnnotationClient } from './KGAnnotationClient.mjs';
 import * as httpserver from './HTTPServer.mjs';
 import { AuthService } from './auth/AuthService.mjs';
 import { SessionStorePostgres } from './auth/SessionStorePostgres.mjs';
+import { UserStorePostgres } from './users/UserStorePostgres.mjs';
 
 // Load the config asap as basically everything depends on it
 const SERVER_CONFIG = await loadConfigFromFile(process.argv.length < 3 ? './configurations/mock.json' : './' + process.argv[2]);
@@ -41,6 +42,9 @@ const AUTH_SERVICE = new AuthService({
     sessionAbsoluteTTLSec: SERVER_CONFIG.sessions.session_absolute_ttl_sec,
     sessionMaxIdleTimeSec: SERVER_CONFIG.sessions.session_max_idle_time_sec
   }, new SessionStorePostgres({
+    ...SERVER_CONFIG.storage.sessions_pg,
+    password: SERVER_CONFIG.secrets.storage.sessions_pg.password
+  }), new UserStorePostgres({
     ...SERVER_CONFIG.storage.sessions_pg,
     password: SERVER_CONFIG.secrets.storage.sessions_pg.password
   })
