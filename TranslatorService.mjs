b6807451373476a0d2977349eea2f1921b72df5c
Guthrie Price <guthrie.price@bmahq.com>
2023-08-02 13:42:43 -0700

    switch to merged ARS results

--- TranslatorService.mjs
+++ TranslatorService.mjs
@@ -62,7 +62,7 @@ class TranslatorService
   {
     try
     {
-      let res = await this.queryClient.collectAllResults(queryId, filters, true);
+      let res = await this.queryClient.collectMergedResults(queryId, filters, true);
       return res;
     }
     catch (err)
