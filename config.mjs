86cd284452781d1ae59886ff01e2086dc820a752
Nishad Prakash <nishad@gmail.com>
2023-08-17 15:44:03 +0100

    Fix up auth settings per env; also introduce _load_ functionality for config loading to indicate a file to be loaded

--- config.mjs
+++ config.mjs
@@ -2,9 +2,34 @@
 
 import * as cmn from './common.mjs';
 
-export { loadConfigFromFile };
+export { loadConfigFromFile, postProcessConfig };
+
 
 async function loadConfigFromFile(filePath) {
+  console.log(filePath);
+  let config = await cmn.readJson(filePath);
+  if (!config['document-root'])
+  {
+    config['document-root'] = process.cwd();
+  }
+
+  for (let k of Object.keys(config)) {
+    if (k.match(/^_load_/)) {
+      await loadAndReplace(config, k);
+    }
+  }
+  console.log(config);
+
+  return config;
+}
+
+// Hack city
+function postProcessConfig(config) {
+  // Put the host specified at top-level config into the pg-specific object
+  config.storage.pg.host = config.pg_host;
+}
+
+async function loadConfigFromFile2(filePath) {
   console.log(filePath);
   let config = await cmn.readJson(filePath);
   if (!config['document-root'])
@@ -28,7 +53,17 @@ async function loadConfigFromFile(filePath) {
   return config;
 }
 
+
+
 async function loadAndReplace(config, prop)
+{
+  if (config[prop])
+  {
+    config[prop.replace(/^_load_/, '')] = await cmn.readJson(config[prop]);
+  }
+}
+
+async function loadAndReplace2(config, prop)
 {
   if (config[prop])
   {
