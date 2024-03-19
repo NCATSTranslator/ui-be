b9d81dc409238f25509091e22391cce134528e2d
Guthrie Price <guthrie.price@bmahq.com>
2024-03-18 17:15:35 -0700

    update infores catalog; update infores utility

--- utilities/updateInfores.sh
+++ utilities/updateInfores.sh
@@ -6,7 +6,7 @@ if [ -z "${version}" ]; then
   version='master'
 fi
 
-curl -X 'GET' "https://raw.githubusercontent.com/biolink/biolink-model/${version}/infores_catalog.yaml" > 'tmp.yaml'
+curl -X 'GET' "https://raw.githubusercontent.com/biolink/information-resource-registry/${version}/infores_catalog.yaml" > 'tmp.yaml'
 yq -o=json 'tmp.yaml' > 'tmp.json'
 node ./node/inforesToInforesMini.mjs 'tmp.json' > "${infores_catalog}"
 rm tmp.yaml tmp.json
