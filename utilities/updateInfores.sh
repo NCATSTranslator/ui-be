2144b6a1c1f151528afc5a7f305c9aa461554d9a
Guthrie Price <guthrie.price@bmahq.com>
2024-03-18 17:18:12 -0700

    reduce script complexity

--- utilities/updateInfores.sh
+++ utilities/updateInfores.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-infores_catalog=$1
-version=$2
+version=$1
 if [ -z "${version}" ]; then
-  version='master'
+  version='main'
 fi
 
 curl -X 'GET' "https://raw.githubusercontent.com/biolink/information-resource-registry/${version}/infores_catalog.yaml" > 'tmp.yaml'
 yq -o=json 'tmp.yaml' > 'tmp.json'
-node ./node/inforesToInforesMini.mjs 'tmp.json' > "${infores_catalog}"
+node ./node/inforesToInforesMini.mjs 'tmp.json' > "infores-catalog-${version}.json"
 rm tmp.yaml tmp.json
