a8ced7e55ea8277820752b097cc9b7aa741b990c
Nishad Prakash <nishad@gmail.com>
2024-03-31 18:41:04 -0400

    Add controller classes for the query and config API, and for login/logout.
    When an object method is passed as a callback, the parent object is no
    longer available via `this`. The use of an explicit bind() call in the
    router is how one works around this.
    
    It's a little ugly, but it allows us to have controllers share access to
    the services/stores across their handlers without resorting to worse
    access patters like a singleton service locator, etc.

--- controllers/ConfigAPIController.mjs
+++ controllers/ConfigAPIController.mjs
@@ -1,9 +1,22 @@
 'use strict';
 
-import { default as express } from 'express';
+export { ConfigAPIController };
 
-export { configAPIController  };
+class ConfigAPIController {
+  constructor(config) {
+    this.config = config;
+  }
 
+  getConfig(req, res, next) {
+    return res.status(200).json({
+      gaID: this.config.google_analytics_id,
+      cached_queries: this.config.frontend.cached_queries.filter(e => e.allow_outbound),
+      name_resolver: this.config.frontend.name_resolver.endpoint,
+      social_providers: this.config.auth.social_providers
+    });
+  }
+}
+/*
 function configAPIController(config) {
   const router = express.Router();
   router.get('/', function(req, res, next) {
@@ -15,4 +28,4 @@ function configAPIController(config) {
     });
   });
   return router;
-}
+}*/
