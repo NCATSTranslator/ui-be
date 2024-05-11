9ed82f7d0eec853f6c819a9cc8ae61072daebb6e
Nishad Prakash <nishad@gmail.com>
2024-05-11 15:22:50 -0400

    Savework

--- controllers/SessionController.mjs
+++ controllers/SessionController.mjs
@@ -1,5 +1,8 @@
 'use strict';
 
+import * as AuthService from '../services/AuthService.mjs';
+import * as wutil from '../lib/webutils.mjs';
+
 export { SessionController };
 
 class SessionController {
@@ -8,11 +11,66 @@ class SessionController {
     this.authService = authService;
   }
 
-  async getStatus(req, res, next) {
+  // All subsequent Session Controller middleware functions assume that this has been done
+  async attachSessionStatus(req, res, next) {
+    let sessionStatus = await this._fetchStatus(req);
+    if (!sessionStatus) {
+      return res.status(500).send(`Server error retrieving session status.`);
+    }
+    req.sessionStatus = sessionStatus;
+    next();
+  }
+
+  async _fetchStatus(req) {
     let cookieToken = req.cookies[this.config.session_cookie.name];
-    let sessionStatus = await this.authService.getSessionStatus(cookieToken);
-    delete sessionStatus.session.data; // remove raw oauth data
-    return res.status(200).json(sessionStatus);
+    let retval = await this.authService.getSessionStatus(cookieToken);
+    console.log(retval);
+    return retval;
+  }
+
+  async getStatus(req, res, next) {
+    if (!req.sessionStatus) {
+      return res.status(500).send(`Server error retrieving session status`);
+    }
+    let sessionStatus = {...req.sessionStatus};
+     return res.status(200).json(this._sanitizeSessionStatus(sessionStatus));
+  }
+
+  async refreshSession(req, res, next) {
+    let sessionStatus = req.sessionStatus;
+    let cookiePath = '/';
+    let cookieMaxAgeSec = this.authService.sessionAbsoluteTTLSec;
+    let success = false;
+    let newSession;
+    switch (sessionStatus.status) {
+      case AuthService.SESSION_TOKEN_EXPIRED:
+        newSession = await this.authService.refreshSessionToken(sessionStatus.session);
+        if (!newSession) {
+          return res.status(500).send("Server error refreshing session token");
+        }
+        success = true;
+        wutil.setSessionCookie(res, this.config.session_cookie, newSession.token, cookiePath, cookieMaxAgeSec);
+        req.sessionStatus = newSession;
+        break;
+      case AuthService.SESSION_VALID:
+        newSession = await this.authService.updateSessionTime(sessionStatus.session);
+        if (!newSession) {
+          return res.status(500).send("Server error updating session time.");
+        }
+        req.sessionStatus = newSession;
+        break;
+      default:
+        success = false;
+        break;
+    }
+    next();
+  }
+
+  _sanitizeSessionStatus(sessionStatus) {
+    if (sessionStatus && sessionStatus.session && sessionStatus.session.data) {
+      delete sessionStatus.session.data;
+    }
+    return sessionStatus;
   }
 
   async updateStatus(req, res, next) {
