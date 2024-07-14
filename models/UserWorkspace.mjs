b507644ee8e5a0166afc6d9e231dc2fb076b41db
Nishad Prakash <nishad@gmail.com>
2024-07-14 17:52:59 -0400

    Add a model class for workspaces

---
+++ models/UserWorkspace.mjs
@@ -0,0 +1,49 @@
+'use strict';
+import { v4 as uuidv4 } from 'uuid';
+import { generateRandomString } from '../lib/common.mjs';
+export { UserWorkspace };
+
+class UserWorkspace {
+  constructor({
+    id = uuidv4(),
+    name,
+    description = null,
+    label = generateRandomString(3,4),
+    user_id,
+    deleted = false,
+    is_public = false,
+    time_created = new Date(),
+    time_updated = new Date(),
+    data = null,
+  } = {}) {
+    if (!(user_id && name)) {
+      throw new Error('user_id and name are both required');
+    }
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.label = label;
+    this.user_id = user_id;
+    this.deleted = deleted;
+    this.is_public = is_public;
+    this.time_created = time_created;
+    this.time_updated = time_updated;
+    this.data = data;
+  }
+
+  update(fields) {
+    for (let key in fields) {
+      if (this.hasOwnProperty(key)) {
+        this[key] = fields[key];
+      }
+    }
+    this.time_updated = new Date();
+    return this;
+  }
+
+  delete() {
+    this.deleted = true;
+    this.time_updated = new Date();
+    return this;
+  }
+}
