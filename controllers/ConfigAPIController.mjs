2ae2bd378c75112d82923d8501b88052f68d4a20
Nishad Prakash <nishad@gmail.com>
2024-08-11 16:24:35 -0400

    Remove dead code

--- controllers/ConfigAPIController.mjs
+++ controllers/ConfigAPIController.mjs
@@ -16,16 +16,3 @@ class ConfigAPIController {
     });
   }
 }
-/*
-function configAPIController(config) {
-  const router = express.Router();
-  router.get('/', function(req, res, next) {
-    return res.status(200).json({
-      gaID: config.google_analytics_id,
-      cached_queries: config.frontend.cached_queries.filter(e => e.allow_outbound),
-      name_resolver: config.frontend.name_resolver.endpoint,
-      social_providers: config.auth.social_providers
-    });
-  });
-  return router;
-}*/
