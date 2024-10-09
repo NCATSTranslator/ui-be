8878387dbb24d419856853c8101054cda7e87129
David Smith <dnsmith124@gmail.com>
2024-10-09 13:08:24 -0500

    add pf config value to config endpoint

--- controllers/ConfigAPIController.mjs
+++ controllers/ConfigAPIController.mjs
@@ -12,7 +12,8 @@ class ConfigAPIController {
       gaID: this.config.google_analytics_id,
       cached_queries: this.config.frontend.cached_queries.filter(e => e.allow_outbound),
       name_resolver: this.config.frontend.name_resolver.endpoint,
-      social_providers: this.config.auth.social_providers
+      social_providers: this.config.auth.social_providers,
+      include_pathfinder: this.config.auth.include_pathfinder
     });
   }
 }
