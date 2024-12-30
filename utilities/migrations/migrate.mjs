7d22b45d06542657cfd24da898d1e2089c8c5b7f
Nishad Prakash <nishad@gmail.com>
2024-12-29 22:00:45 -0500

    Load config and establish DB conn

--- utilities/migrations/migrate.mjs
+++ utilities/migrations/migrate.mjs
@@ -1,6 +1,65 @@
 'use strict';
 
 import { readdir } from 'node:fs/promises';
+import path from 'path';
+import { bootstrapConfig } from '../../lib/config.mjs';
+import { pg } from '../../lib/postgres_preamble.mjs';
+
+import meow from 'meow';
+
+export { getMigrationFiles };
+
+const program_name = path.basename(process.argv[1]);
+const cli = meow(`
+    Usage:
+    $ node ${program_name} <options>
+
+    Options:
+      --config-file, -c: required
+      --local-overrides, -l: optional
+      --one-big-tx, -b: boolean
+      --force-dangerous-things: true
+      --start-at: number, optional
+      --stop-after: number, optional
+    `, {
+        importMeta: import.meta,
+        allowUnknownFlags: false,
+        flags: {
+            configFile: {
+                type: 'string',
+                shortFlag: 'c',
+                isRequired: true
+            },
+            localOverrides: {
+                type: 'string',
+                shortFlag: 'l',
+                isRequired: false
+            },
+            oneBigTx: {
+                type: 'boolean',
+                default: true,
+                isRequired: false
+            },
+            forceDangerousThings: {
+                type: 'boolean',
+                default: false,
+                isRequired: false
+            },
+            startAt: {
+                type: 'number',
+                isRequired: false
+            },
+            stopAfter: {
+                type: 'number',
+                isRequired: false
+            }
+        }
+    }
+);
+
+console.log(cli.flags);
+
+
 
 async function getMigrationFiles(dir, after_timestamp=0, suffix='.sql') {
     let match_rx = new RegExp(`^(\\d+)${suffix}$`); // Note doubled \\
@@ -19,6 +78,18 @@ async function getMigrationFiles(dir, after_timestamp=0, suffix='.sql') {
     });
 }
 
-export { getMigrationFiles };
+async function update_migrations_table(applied_migrations) {
+
+}
 
 
+const CONFIG = await bootstrapConfig(cli.flags.configFile, cli.flags.localOverrides ?? null);
+console.log(CONFIG);
+
+const dbPool = new pg.Pool({
+    ...CONFIG.storage.pg,
+    password: CONFIG.secrets.pg.password,
+    ssl: CONFIG.db_conn.ssl
+  });
+
+console.log(dbPool);
