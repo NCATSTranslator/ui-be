e698c5f20a7f3913f32775414760919c9a67dd2d
Guthrie Price <guthrie.price@bmahq.com>
2025-01-17 14:24:30 -0800

    Update infores ingest tools to allow for url injection

--- utilities/node/inforesToInforesMini.mjs
+++ utilities/node/inforesToInforesMini.mjs
@@ -18,18 +18,31 @@ function cleanupKnowledgeLevel(rawKL) {
 }
 
 const filePath = process.argv[2];
+const patchPath = process.argv[3];
 const inforesCatalog = await cmn.readJson(filePath);
 const inforesEntries = inforesCatalog.information_resources;
 const inforesMini = {};
 inforesEntries.forEach((inforesEntry) => {
-  if (inforesEntry.id)
-  {
+  if (inforesEntry.id) {
     inforesMini[inforesEntry.id] = {
       name: inforesEntry.name || inforesEntry.id,
-      url: (inforesEntry.xref && inforesEntry.xref.length > 0) ? inforesEntry.xref[0] : null,
+      wiki: (inforesEntry.xref && inforesEntry.xref.length > 0) ? inforesEntry.xref[0] : null,
       knowledge_level: cleanupKnowledgeLevel(inforesEntry['knowledge level'])
     };
   }
 });
+
+if (patchPath) {
+  const patchFile = await cmn.readJson(patchPath);
+  for (const entryPatch of patchFile) {
+    const entry = inforesMini[entryPatch.id];
+    if (entry) {
+      for (const key in entryPatch) {
+        if (key === 'id') continue;
+        entry[key] = entryPatch[key];
+      }
+    }
+  }
+}
 
 console.log(JSON.stringify(inforesMini));
