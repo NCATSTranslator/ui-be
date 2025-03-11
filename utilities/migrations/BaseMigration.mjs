273b88d2c2cff27d8401873c90e09fc8f8e81720
Nishad Prakash <nishad@gmail.com>
2025-03-11 09:56:43 -0400

    Add a DB migration tool

---
+++ utilities/migrations/BaseMigration.mjs
@@ -0,0 +1,29 @@
+'use strict';
+
+import { pg, pgExec } from '../../lib/postgres_preamble.mjs';
+
+export { BaseMigration };
+
+class BaseMigration {
+    constructor(dbPool) {
+        this.dbPool = dbPool;
+        this.sql = [];
+    }
+
+    async execute(obj=null) {
+        for (let stmt of this.sql) {
+            let res = await pgExec(this.dbPool, stmt);
+        };
+        return true;
+    }
+
+    async verify(obj=null) {
+        // Do not implement--derived classes should override
+        throw new Error('Not implemented');
+    }
+
+    success_message(obj=null) {
+        // Do not implement--derived classes should override
+        throw new Error('Not implemented');
+    }
+}
