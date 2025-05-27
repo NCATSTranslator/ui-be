78e3fae1fd2b21c55843f24d295aa5ac302ddf61
Guthrie Price <guthrie.price@bmahq.com>
2025-05-27 15:46:18 -0700

    Implement recursive path generation

--- test/summary-analysis.mjs
+++ test/summary-analysis.mjs
@@ -1,12 +1,23 @@
 import * as cmn from '../lib/common.mjs';
 import * as bl from '../lib/biolink-model.mjs';
 import * as test from './lib/common.mjs';
-import { analysis_to_summary_analysis } from '../lib/summarization/summary-analysis.mjs';
+import * as sa from '../lib/summarization/summary-analysis.mjs';
 
 async function test_summary_analysis(root_path) {
-  await test.functionalTest(analysis_to_summary_analysis,
+  await test.functionalTest(sa.analysis_to_summary_analysis,
                             await cmn.readJson(`${root_path}/analysis_to_summary_analysis.json`),
-                            bl.loadBiolink)
+                            bl.loadBiolink);
+  await test.functionalTest(sa.gen_analysis_summary_paths,
+                            await cmn.readJson(`${root_path}/gen_analysis_summary_paths.json`),
+                            null,
+                            async (args) => {
+                              const key = args[0];
+                              args[0] = await test.runCase(sa.analysis_to_summary_analysis,
+                                await cmn.readJson(`${root_path}/analysis_to_summary_analysis.json`),
+                                key,
+                                bl.loadBiolink);
+                              return args;
+                            });
 }
 
 const root_path = './test/data/summary-analysis';
