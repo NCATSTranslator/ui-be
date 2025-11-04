438337e3c743aa16c9084b82117e132084f54a99
Guthrie Price <guthrie.price@bmahq.com>
2025-11-04 12:17:18 -0800

    Start refactoring summarization property rules

--- test/summarization/property-rules.mjs
+++ test/summarization/property-rules.mjs
@@ -8,6 +8,16 @@ async function _test_summarization_property_rules(root_path) {
     test_cases: await cmn.readJson(`${root_path}/make_rule_group_publications_by_knowledge_level.json`),
     post_func: test.apply_rule
   });
+  await test.functional_test({
+    test_func: summary_rules.make_rule_collect_publication_supporting_text,
+    test_cases: await cmn.readJson(`${root_path}/make_rule_collect_publication_supporting_text.json`),
+    post_func: test.apply_rule
+  });
+  await test.functional_test({
+    test_func: summary_rules.make_rule_collect_semmed_sentences,
+    test_cases: await cmn.readJson(`${root_path}/make_rule_collect_semmed_sentences`),
+    post_func: test.apply_rule
+  });
 }
 
 const root_path = './test/data/summarization';
